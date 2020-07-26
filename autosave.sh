9b2c7b5bf1f4dd22f4b18abd69a1b6c3d0ca67e1
Ade <adelphos@msn.cn>
2020-07-26 16:25:26 +0800

    auto save from mac pro

---
+++ autosave.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /etc/profile
+echo -------------
+echo $PATH
+echo ----------
+PATH = $PATH:/usr/local/bin
+
+git add .
+git commit -am "auto save from mac pro"
+git push origin master
+
+
