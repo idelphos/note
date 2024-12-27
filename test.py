55cc56a5fe09b37643f20c4d52160fcb2b0ac888
idelphos <adelphos@msn.cn>
2024-12-27 13:04:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 
+
+
+for cock in range(0,20):
+    for hen in range(0,33):
+        chicken = 100 - cock - hen
+        if cock * 5 + hen * 3 + chicken/3 == 100:
+            print(f'the amount of cock is {cock}, the amount of hen is {hen}, the amount of chicken is {chicken}')
