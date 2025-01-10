bea2356bc6ae44997933c1a358d54d69a9cdfea7
idelphos <adelphos@msn.cn>
2025-01-10 10:23:53 +0800

    t

--- temp.sql
+++ temp.sql
@@ -137,7 +137,7 @@ CREATE INDEX index_name
 ON table_name (column1 [ASC|DESC], column2 [ASC|DESC], ...);
 
 
-CREATE TABEL table_name(
+CREATE TABLE table_name(
     属性名 数据类型,
     属性名 数据类型,
     ....
@@ -161,10 +161,10 @@ Create Table: CREATE TABLE `t_dept` (
   KEY `index_deptno` (`deptno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-mysql> EXPLAIN select * from t_dept where deptno=1 \G
+mysql> EXPLAIN SELECT * FROM t_dept WHERE deptno=1 \G
 *************************** 1. row ***************************
            id: 1
-  select_type: SIMPLE
+  SELECT_type: SIMPLE
         table: t_dept
    partitions: NULL
          type: ref
@@ -205,7 +205,7 @@ EXPLAIN SELECT * FROM t_dept WHERE dname='cjgong' \G
 
 CREATE VIEW view_name
     AS 
-        select-statement
+        SELECT-statement
 
 
 CREATE table t_product(
@@ -220,12 +220,12 @@ INSERT INTO t_product(name,price)
         ('orange',1.5),
         ('pear',2.5);
 
-CREATE VIEW v_selectproduct
+CREATE VIEW v_SELECTproduct
     AS 
         SELECT id,name 
             FROM t_product;
 
-mysql> select * from v_selectproduct;
+mysql> SELECT * FROM v_SELECTproduct;
 +----+--------+
 | id | name   |
 +----+--------+
@@ -250,25 +250,379 @@ CREATE TABLE t_student(
 
 
 INSERT INTO t_student(name,sex,group_id)
-   VALUES (cjgong1,'M',1),
-          (cjgong1,'M',1),
-          (cjgong1,'M',1),
-          (cjgong1,'M',1),
-          (cjgong1,'M',1),
-          (cjgong1,'M',1),
-          (cjgong1,'M',1),
-          (cjgong1,'M',1),
-          (cjgong1,'M',1),
-
-          
+   VALUES ('cjgong1','M',1),
+          ('cjgong2','M',1),
+          ('cjgong3','W',2),
+          ('cjgong4','M',2),
+          ('cjgong5','W',2),
+          ('cjgong6','M',3),
+          ('cjgong7','M',5),
+          ('cjgong8','W',4),
+          ('cjgong9','M',3);
 
 
+CREATE VIEW v_test3
+    AS 
+        SELECT name FROM t_student
+            ORDER BY id DESC;
+
+INSERT INTO v_product(name)
+    VALUES('pear5'),
+          ('pear6')
 
 
 
+SELECT * FROM v_product
+    WHERE name LIKE 'pear%';
+
+DELETE FROM v_product
+    WHERE name LIKE 'pear%';
+
+ALTER VIEW v_product 
+    AS SELECT id,name,price FROM t_product;
+UPDATE v_product
+    set price=8.8
+        WHERE name='apple';
+
+
+CREATE TABLE t_dept
+
+CREATE TABLE t_diary(
+    diaryno INT PRIMARY KEY AUTO_INCREMENT,
+    tablename   VARCHAR(20),
+    diarytime   DATETIME
+);
+
+CREATE TRIGGER tri_diarytime
+    BEFORE INSERT 
+        ON t_dept FOR EACH ROW
+            INSERT INTO t_diary 
+                VALUES(NULL,'t_dept',now());
+
+INSERT INTO t_dept ()
+    VALUES (1,'cjgongdept','ShangXi');
+
+INSERT INTO table_name(field1,field2...fieldn)
+    VALUES(value1,value2...valuen)
+
+INSERT INTO t_dept
+    VALUES(5,'f_dept','XiAn'),
+        (6,'d_dept','ShenZhen'),
+        (9,'x_dept','GuangZhou');
+
+INSERT INTO table_name(field1,field2...fieldn)
+    SELECT (field21,field22...field2n)
+        FROM table_name2
+            WHERE 
+
+CREATE TABLE t_loader(
+    id INT,
+    name VARCHAR(20),
+    dname VARCHAR(20),
+    loc VARCHAR(40)
+);
+
+
+INSERT INTO t_loader
+    VALUES(1,'cjgong1','dept1','shangxi1'),
+           (2,'cjgong2','dept2',NULL),
+           (3,'cjgong3','dept3','shangxi3'),
+           (4,'cjgong4','dept4','shangxi4'),
+           (5,'cjgong5','dept5','shangxi5');
+
+INSERT INTO t_dept(dname,loc)
+    SELECT dname,loc
+        FROM t_loader;
+
+UPDATE t_dept
+    SET loc='shangxi2'
+    WHERE dname='cjgongdept';
+
+UPDATE t_dept
+    SET dname='w_dept',
+        loc='WuHan'
+    WHERE deptno=10;
+
+UPDATE t_dept
+    SET dname='dept_test',
+        loc='loc_test'
+    WHERE dname LIKE 'dept%';
+
+DELETE FROM table_name
+    [WHERE condition]
+
+SELECT CONCAT(a.first_name,"的年薪是: ", b.salary*12)
+    FROM employees AS a, salaries AS b
+        WHERE a.emp_no = b.emp_no
+        LIMIT 10;
+
+SELECT emp_no,first_name,last_name
+    FROM employees
+        WHERE first_name = 'Prasadram' && gender = 'M';
+
+SELECT emp_no,first_name,last_name
+    FROM employees
+        WHERE hire_date BETWEEN '2000-01-01' AND '2008-01-06';
+
+SELECT emp_no,first_name,last_name
+    FROM employees
+        WHERE emp_no NOT BETWEEN 200000 AND 500000;
+
+SELECT *
+    FROM t_loader
+        WHERE loc IS NULL;
+
+SELECT emp_no,first_name,last_name
+    FROM employees
+        WHERE emp_no IN (100000,200000,300000,400000,500000);
+
+INSERT INTO employees(birth_date, first_name,last_name,gender,hire_date)
+    VALUES('19861213','Ade','Yuan','M','20100102');
 
 
 
+mysql> SELECT * FROM t_loader;
++------+---------+-------+----------+
+| id   | name    | dname | loc      |
++------+---------+-------+----------+
+|    1 | cjgong1 | dept1 | shangxi1 |
+|    2 | cjgong2 | dept2 | NULL     |
+|    3 | cjgong3 | dept3 | shangxi3 |
+|    4 | cjgong4 | dept4 | shangxi4 |
+|    5 | cjgong5 | dept5 | shangxi5 |
++------+---------+-------+----------+
+5 rows in set (0.00 sec)
+
+mysql> SELECT * FROM t_loader WHERE loc IN('shangxi1',NULL);
++------+---------+-------+----------+
+| id   | name    | dname | loc      |
++------+---------+-------+----------+
+|    1 | cjgong1 | dept1 | shangxi1 |
++------+---------+-------+----------+
+
+mysql> SELECT * FROM t_loader WHERE loc NOT IN('shangxi1',NULL);
+Empty set (0.00 sec)
+
+SELECT * 
+    FROM table_name
+        ORDER by fieldx;
+
+SELECT * 
+    FROM table_name
+        ORDER by field1 ASC,
+                field2 DESC;
+
+SELECT * 
+    FROM table_name
+        WHERE condition
+            LIMIT ROW_COUNT;
+
+SELECT COUNT(*)
+    FROM t_loader;
+
+SELECT COUNT(loc)
+    FROM t_loader;
+
+SELECT COUNT(loc)
+    FROM t_loader
+        WHERE loc LIKE 'shangxi%';
+
+SELECT function()
+    FROM table_name
+        WHERE condition
+            GROUP BY field;
+
+SELECT dept_no, COUNT(emp_no), GROUP_CONCAT(emp_no) emp_no
+    FROM dept_emp 
+        GROUP BY dept_no;
 
 
+SELECT GROUP_CONCAT(field), function(field)
+    FROM table_name
+        WHERE condition
+            GROUP BY field1,field2..
 
+SELECT function(field)
+    FROM table_name
+        WHERE condition
+        GROUP by filed1, field2...
+        HAVING condition;
+
+SELECT depno,AVG(sal) average_sal,GROUP_CONCAT(ename) ename,count(ename)
+    FROM t_employee
+    GROUP by deptno
+    HAVING AVG(sal) > 2000;
+
+INSERT INTO t_dept
+    VALUES(10,'ACCOUNTING','NEW YORK'),
+          (20,'RESEARCH','DALLAS'),
+          (30,'SALES','CHICAGO'),
+          (40,'OPERATIONS','BOSTON');
+
+CREATE TABLE t_employee(
+    empno INT PRIMARY KEY,
+    ename VARCHAR(20),
+    job VARCHAR(20),
+    MGR INT,
+    Hiredate DATE,
+    sal FLOAT,
+    comm FLOAT,
+    deptno INT,
+    CONSTRAINT fk_deptno FOREIGN KEY (deptno) REFERENCES t_dept (deptno)
+);
+
+INSERT INTO t_employee
+    VALUES (7369,'SMITH','CLERK',7902,'19810312',800,NULL,20),
+        (7499,'ALLEN',"SALESMAN",7698,'19820312',1600,300,30),
+        (7521,'WARD','SALESMAN',7698,'19830312',1250,500,30),
+        (7566,'JONES','MANAGER',7839,'19810312',2975,NULL,20),
+        (7654,'MARTIN','SALESMAN',7698,'19810312',1250,1400,30),
+        (7698,'BLAKE','MANAGER',7839,'19850312',2850,NULL,30),
+        (7782,'CLARK','MANAGER',7839,'19850312',2450,NULL,10),
+        (7788,'SCOTT','ANALYST',7566,'19810312',3000,NULL,20),
+        (7839,'KING','PRESIDENT',NULL,'19820312',5000,NULL,10),
+        (7844,'TURNER','SALESMAN',7698,'19890323', 1500,0,30),
+        (7876,'ADAMS','CLERK',7788,'19871213',1100,NULL,20),
+        (7900,'JAMES','CLERK',7698,'19870312',950,NULL,30),
+        (7902,'FORD','ANALYST',7566,NULL,3000,NULL,20),
+        (7934,'MILLER','CLERK',7782,'19821204',1300,NULL,10);
+
+SELECT field1, field2 ...
+    FROM table_name1 INNER JOIN table_name2 [INNER JOIN table_name3]
+    ON join_condition
+
+SELECT e.ename employname, e.job
+    FROM t_employee e;
+
+SELECT e.ename employeename, e.job, l.ename managername 
+    FROM t_employee e INNER JOIN t_employee l
+        ON e.mgr=l.empno;
+
+
+SELECT e.empno,e.ename,e.job,d.dname,d.loc
+    FROM t_employee e INNER JOIN t_dept d
+        ON e.deptno = d.deptno;
+
+SELECT e.empno,e.ename,e.job,d.dname,d.loc
+    FROM t_employee e, t_dept d
+        WHERE e.deptno = d.deptno;
+
+
+SELECT e.empno,e.ename,e.sal,e.job, l.ename manager,d.dname,d.loc
+    FROM t_employee e INNER JOIN t_employee l ON e.mgr = l.empno
+        INNER JOIN t_dept d ON e.deptno = d.deptno;
+
+SELECT field1, field2,...
+    FROM table_name1 LEFT|RIGHT|FULL [OUTER] JOIN table_name2 
+        ON join_condition;
+        
+INSERT INTO t_employee
+    VALUES (7999,'cjgong','CLERK',7782,'19810312',5200,NULL,50);
+
+
+SELECT e.ename,e.job 
+    FROM t_employee;
+
+SELECT e.ename, e.job,l.ename
+    FROM t_employee e LEFT JOIN t_employee l
+        ON e.mgr = l.empno;
+
+SELECT field1,field2... fieldn
+    FROM table_name
+UNION | UNION ALL
+SELECT field1,field2... fieldn
+    FROM table_name
+
+
+SELECT sal
+    FROM t_employee
+        WHERE ename='SMITH';
+
+SELECT * 
+    FROM t_employee
+    WHERE sal > (
+        SELECT sal
+            FROM t_employee
+            WHERE ename='SMITH'
+    );
+
+SELECT sal,job
+    FROM t_employee
+    WHERE ename = 'SMITH';
+
+SELECT ename,sal,job
+    FROM t_employee
+    WHERE (sal,job)=(
+        SELECT sal,job
+            FROM t_employee
+            WHERE ename = 'SMITH'
+    );
+
+SELECT deptno
+    FROM t_dept;
+
+SELECT * 
+    FROM t_employee
+    WHERE deptno IN (
+        SELECT deptno
+            FROM t_dept
+    );
+
+SELECT sal
+    FROM t_employee
+    WHERE job='MANAGER';
+
+SELECT ename,sal
+    FROM t_employee
+    WHERE sal > ANY(
+        SELECT sal
+            FROM t_employee
+            WHERE job='MANAGER'
+    );
+
+
+SELECT * 
+    FROM t_employee a, t_dept c 
+    WHERE a.deptno = c.deptno;
+
+SELECT * 
+    FROM t_dept c
+    WHERE NOT EXISTS(
+        SELECT *
+            FROM t_employee
+            WHERE deptno=c.deptno
+    );
+
+SELECT d.deptno,d.dname,d.loc,COUNT(e.empno),AVG(e.sal) average_sal
+    FROM t_employee e INNER JOIN t_dept d 
+        ON e.deptno = d.deptno
+        GROUP by d.deptno  , d.dname,d.loc;
+
+CREATE PROCEDURE procedure_name (parameter list)
+    routine_body
+
+CREATE FUNCTION function_name(parameter list)
+    routine_body
+
+
+DELIMITER $$
+CREATE PROCEDURE proce_employee_sal()
+COMMENT '查询所有雇员的工资'
+BEGIN
+    SELECT sal
+        FROM t_employee
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION func_employee_sal(empno INT(11))
+    RETURNS DOUBLE(10,2)
+COMMENT '查询某个雇员的工资'
+BEGIN
+    RETURN (
+        SELECT sal
+            FROM t_employee
+            WHERE t_employee.empno=empno);
+    )
+END$$
+DELIMITER ;
